9056dee55e08ae0387c9b889464ccff30f7d79de
Thane Thomson <connect@thanethomson.com>
2017-01-30 14:07:11 +0200

    setting version to v0.1.0

---
+++ mlalchemy/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+from mlalchemy.constants import *
+from mlalchemy.errors import *
+from mlalchemy.structures import *
+from mlalchemy.parser import *
+
+
+__version__ = "0.1.0"
